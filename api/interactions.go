0afa110ff78d5755406106f6e3764ebf807eb3fd
TopiSenpai <tobi@strasserhof.com>
2022-07-28 02:17:19 +0200

    update stuff

--- api/interactions.go
+++ api/interactions.go
@@ -8,24 +8,25 @@ import (
 
 	"github.com/disgoorg/disgo/discord"
 	"github.com/disgoorg/disgo/httpserver"
+	"github.com/disgoorg/disgo/rest"
 	"github.com/disgoorg/log"
 	"vercel-test-bot/commands"
 )
 
-var handlers = map[string]func(interaction discord.ApplicationCommandInteraction, done func()) discord.InteractionResponse{
+var handlers = map[string]func(client rest.Rest, interaction discord.ApplicationCommandInteraction) discord.InteractionResponse{
 	"ping": commands.PingCommandHandler,
 }
 
 func HandleInteractions(w http.ResponseWriter, r *http.Request) {
 	logger := log.Default()
 	logger.SetLevel(log.LevelTrace)
-	logger.Infof("Received interaction request: %s", r.URL.Path)
+	client := rest.New(rest.NewClient(""))
+
 	publicKey, err := hex.DecodeString(os.Getenv("PUBLIC_KEY"))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	var done chan struct{}
 	httpserver.HandleInteraction(publicKey, logger, func(respondFunc httpserver.RespondFunc, event httpserver.EventInteractionCreate) {
 		switch i := event.Interaction.(type) {
 		case discord.PingInteraction:
@@ -34,19 +35,15 @@ func HandleInteractions(w http.ResponseWriter, r *http.Request) {
 			})
 
 		case discord.ApplicationCommandInteraction:
-			done = make(chan struct{}, 1)
 			handler, ok := handlers[i.Data.CommandName()]
 			if !ok {
 				err = fmt.Errorf("command %s not implemented", i.Data.CommandName())
 				break
 			}
-			err = respondFunc(handler(i, func() { done <- struct{}{} }))
+			err = respondFunc(handler(client, i))
 		}
 		if err != nil {
 			logger.Errorf("error while handling interaction: %s", err)
 		}
 	})(w, r)
-	if done != nil {
-		<-done
-	}
 }
--- commands/ping.go
+++ commands/ping.go
@@ -1,13 +1,15 @@
 package commands
 
-import "github.com/disgoorg/disgo/discord"
+import (
+	"github.com/disgoorg/disgo/discord"
+	"github.com/disgoorg/disgo/rest"
+)
 
-func PingCommandHandler(interaction discord.ApplicationCommandInteraction, done func()) discord.InteractionResponse {
-	defer done()
+func PingCommandHandler(client rest.Rest, interaction discord.ApplicationCommandInteraction) discord.InteractionResponse {
 	return discord.InteractionResponse{
 		Type: discord.InteractionResponseTypeCreateMessage,
 		Data: discord.MessageCreate{
-			Content: "Pong!",
+			Content: "Ping!",
 		},
 	}
 }
